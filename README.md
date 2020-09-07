f1a65e23e0425be0aa054adf8f4e14c7e7dc1097
matthewacon <matthewjbarichello@gmail.com>
2020-09-06 23:40:52 -0400

    Added cmake-download.cmake file as well as README info

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # cmake-download
-A simple cmake function to download a cmake-based project without all the fuss of ExternalProject_Add
+A simple cmake function to download a cmake-based project without all the fuss
+of `ExternalProject_Add`.
+
+## Setup 
+### Submodule
+Add this repository as a submodule to you CMake-based repository:
+```sh
+git submodule add git@github.com:Matthewacon/cmake-download.git
+```
+then include the download file in your in your top-level `CMakeLists.txt`:
+```cmake
+include(cmake-download/download.cmake)
+```
+
+### Single file download
+Download the `cmake-download.cmake` file into the root of your repository:
+```sh
+curl https://raw.githubusercontent.com/Matthewacon/cmake-download/master/cmake-download.cmake -o cmake-download.cmake -s
+```
+then include the file in your top-level `CMakeLists.txt`:
+```sh
+include(cmake-download.cmake)
+```
+
+## Usage
+Invoke the `find_or_download` function with the required and any one, or
+more, of the optional arguments:
+| Argument | - | Description |
+| :-- | :-- | :-- |
+| `PACKAGE_NAME` | REQUIRED | The name of the package to find |
+| `VERSION` | OPTIONAL | The version of the package to find |
+| `GIT_REPO` | REQIORED | The repository to download if the package is not found |
+| `GIT_TAG` | OPTIONAL | A branch, tag or commit hash, defaults to 'master' |
+| `STATUS_VAR` | OPTIONAL | The variable to store the download status on, set to `TRUE` if downloaded 
+| `DEPS_DIR` | OPTIONAL | Download destination, defaults to `${CMAKE_SOURCE_DIR}/dependencies` |
+| `DOWNLOAD_OVERRIDE` | OPTIONAL | Force downloading of sources |
+
+### Example
+```cmake
+cmake_minimum_required(VERSION 3.8)
+include(cmake-download/cmake-download.cmake)
+
+find_or_download(
+ PACKAGE_NAME gtest
+ GIT_REPO https://github.com/google/googletest
+ GIT_TAG v1.10.0
+ STATUS_VAR gtest_downloaded
+)
+
+if(${gtest_downloaded})
+ message(STATUS "Building gtest from source")
+ include(dependencies/googletest)
+ #...
+else()
+ message(STATUS "Using system gtest installation")
+ #...
+endif()
+```
+
+## License
+[M.I.T.](https://github.com/Matthewacon/cmake-download/blob/master/LICENSE).
---
+++ cmake-download.cmake
@@ -0,0 +1,89 @@
+cmake_minimum_required(VERSION 3.8)
+
+include(ExternalProject)
+
+function(find_or_download) 
+ cmake_parse_arguments(
+  fol
+  ""
+  "PACKAGE_NAME;VERSION;GIT_REPO;GIT_TAG;STATUS_VAR;DEPS_DIR;DOWNLOAD_OVERRIDE"
+  ""
+  ${ARGN}
+ )
+
+ #Ensure function was invoked correctly
+ if(NOT DEFINED fol_PACKAGE_NAME OR NOT DEFINED fol_GIT_REPO)
+  message(
+   FATAL_ERROR 
+   "'find_or_download' accepts the following named arguments:\ 
+   \n(REQUIRED) 'PACKAGE_NAME' - The name of the package to find\
+   \n(OPTIONAL) 'VERSION' - The version of the package to find\
+   \n(REQUIRED) 'GIT_REPO' - The repository to download if the package is not found\
+   \n(OPTIONAL) 'GIT_TAG' - A branch, tag or commit hash, defaults to 'master'\
+   \n(OPTIONAL) 'STATUS_VAR' - The variable to store the download status in, set to 'TRUE' if downloaded\
+   \n(OPTIONAL) 'DEPS_DIR' - Download destination, defaults to '\${CMAKE_SOURCE_DIR}/dependencies'\
+   \n(OPTIONAL) 'DOWNLOAD_OVERRIDE' - Force downloading of sources\
+  ")
+ endif()
+
+ #Set defualt download destination if not present
+ if(NOT DEFINED fol_DEPS_DIR)
+  set(fol_DEPS_DIR "${CMAKE_SOURCE_DIR}/dependencies")
+ endif()
+
+ #Set default tag if not present
+ if(NOT DEFINED fol_GIT_TAG)
+  set(fol_GIT_TAG "master")
+ endif()
+
+ #Set download override to false if not defined
+ if(NOT DEFINED fol_DOWNLOAD_OVERRIDE)
+  set(fol_DOWNLOAD_OVERRIDE FALSE)
+ endif()
+
+ #For convenience
+ function(set_status_var download_status)
+  if(DEFINED fol_STATUS_VAR)
+   set(download_status ${download_status} PARENT_SCOPE)
+  endif()
+ endfunction()
+
+ #Search for package
+ if(NOT DEFINED fol_DOWNLOAD_OVERRIDE)
+  if(${fol_VERSION})
+   find_package("${fol_PACKAGE_NAME}" "${fol_VERSION}" QUIET)
+  else()
+   find_package("${fol_PACkAGE_NAME}" QUIET)
+  endif()
+ else()
+  set("${fol_PACKAGE_NAME}_FOUND" TRUE)
+ endif()
+
+ #Download if package not found 
+ if("${fol_PACKAGE_NAME}_FOUND" AND NOT ${fol_DOWNLOAD_OVERRIDE})
+  message(STATUS "Found '${fol_PACKAGE_NAME}', not downloading")
+  set_status_var(FALSE)
+ else()
+  message(STATUS "Could not find '${fol_PACKAGE_NAME}', downloading sources")
+  set_status_var(TRUE)
+  if(NOT EXISTS "${fol_DEPS_DIR}/${fol_PACKAGE_NAME}" OR ${fol_DOWNLOAD_OVERRIDE})
+   ExternalProject_Add("${fol_PACKAGE_NAME}"
+    SOURCE_DIR "${fol_DEPS_DIR}/${fol_PACKAGE_NAME}"
+    EXCLUDE_FROM_ALL TRUE
+    STEP_TARGETS download
+    GIT_REPOSITORY "${fol_GIT_REPO}"
+    GIT_TAG "${fol_GIT_TAG}"
+   )
+   #ExternalProject_Add_StepTargets(CX NO_DEPENDS download)
+
+   add_custom_target("download_bootstrap_${fol_PACKAGE_NAME}" ALL DEPENDS "${fol_PACKAGE_NAME}-download") 
+  else()
+   message(STATUS "Found existing sources, not downloading")
+  endif()
+ endif()
+
+ #Propogate status out of the scope of this function
+ if(DEFINED fol_STATUS_VAR)
+  set("${fol_STATUS_VAR}" ${download_status} PARENT_SCOPE)
+ endif()
+endfunction()
